b763c5156f64326f8deaf68bcc4284af7fb12129
Armankhan7890 <mdarmank8287@gmail.com>
2023-10-29 22:01:39 +0530

    Form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+      
+    </div>
+  );
+}
+
+export default App;
